cbe64ce62834351906a3a468c655257a0857728a
JoaPul <joaquinarroyo@outlook.com>
2025-06-22 18:08:19 -0600

    ft:adding and describing register and login routes

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,59 @@
+import { password } from 'bun';
+import { Hono } from 'hono';
+import { z } from 'zod';
+import { email } from 'zod/v4';
+
+export const authRouter = new Hono();
+
+const resterSchema = z.object({
+    email: z.string().trim().toLowerCase().email({
+        message: 'Invalid Email Address',
+    }),
+    password: z.string().min(6, {
+        message: 'Password must be at least 6 characters long',
+    }),
+    username: z.string().min(3, {
+        message: 'Username must be at least 3 characters long'
+    }),
+});
+
+const loginSchema = z.object({
+    email: z.string().trim().toLowerCase().email({
+        message: 'Invalid Email Address',
+    }),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters long",
+    }),
+});
+
+// /api/v1/auth/login
+authRouter.post('/register', async (c) => {
+
+    // get the User data from body
+    const {email, password, username} = await c.req.json();
+
+    // validate the user data from body
+
+
+    // verify that the email is not registered yet
+    // hash the password
+    // create a new user from the data
+
+    // return c.json({message: 'Hello, Login!' });
+    return c.json({email, password });
+});
+
+// /api/v1/auth/register
+authRouter.post('/login', (c) => {
+
+    // get the user data from body
+    // validate the user data
+    // verify that the user is in the data base
+    // verify that the password is correct
+    // create a JWT auth token
+    // return JWT auth token
+
+    return c.json({message: 'Hello,Register!' });
+});
+
+export default authRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,52 @@
 import { Hono } from 'hono';
+import { authRouter } from './routes/auth.route';
 
 const app = new Hono();
 
-app.get("/hello", c => {
-    return c.json({ message : "Hello, World!" });
-});
+/*/ app.get("/hello", (c) => {
+//     return c.json({ message : "Hello, World!" });
+// });
 
-export default app;
+// // returning are application/json
+// app.get('/api/hello', (c) => {
+//     return c.json({
+//         ok: true,
+//         message: 'Hello Hono',
+//     })
+// });
+
+// // getting a path paramater, URL query value, and appending a Response header is written as follows.
+// app.get('/post/:id', (c) => {
+//     const page = c.req.query('page');
+//     const id = c.req.param('id');
+//     c.header('X-Message', 'Hi!');
+//     return c.text(`You want to see ${page} of ${id}`);
+// });
+
+// // handling also POST and DELETE Methods
+// app.post('/post', (c) => c.text('Created!', 201));
+// app.delete('/post/:id', (c) => c.text(`${c.req.param('id')} is deleted`));
+
+/* return a custom HTML
+// const View = () => {
+//     return (
+//         <html>
+//             <body>
+//                 <h1>Hello Hono!</h1>
+//             </body>
+//         </html>
+//     )
+// }
+
+// app.get('/page', (c) => {
+//     return c.html(<View />);
+// });
+*/
+
+app.route('/api/v1/auth', authRouter);
+
+
+export default {
+    port:3000,
+    fetch: app.fetch,
+}
