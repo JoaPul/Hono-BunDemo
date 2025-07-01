d778d28670606940030693eca0fd4ae2577635c9
JoaPul <joaquinarroyo@outlook.com>
2025-06-30 22:15:17 -0600

    ft: adding the zValidator module and run it for register rout without db

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { password } from 'bun';
 import { Hono } from 'hono';
 import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 import { email } from 'zod/v4';
 
 export const authRouter = new Hono();
 
-const resterSchema = z.object({
+const registerSchema = z.object({
     email: z.string().trim().toLowerCase().email({
         message: 'Invalid Email Address',
     }),
@@ -14,7 +15,7 @@ const resterSchema = z.object({
     }),
     username: z.string().min(3, {
         message: 'Username must be at least 3 characters long'
-    }),
+    }).optional(),
 });
 
 const loginSchema = z.object({
@@ -27,24 +28,27 @@ const loginSchema = z.object({
 });
 
 // /api/v1/auth/login
-authRouter.post('/register', async (c) => {
+authRouter.post('/register', 
+    zValidator('json', registerSchema), 
+    async (c) => {
 
     // get the User data from body
     const {email, password, username} = await c.req.json();
 
     // validate the user data from body
-
-
+    
     // verify that the email is not registered yet
     // hash the password
     // create a new user from the data
 
     // return c.json({message: 'Hello, Login!' });
-    return c.json({email, password });
+    return c.json({email, password, username });
 });
 
 // /api/v1/auth/register
-authRouter.post('/login', (c) => {
+authRouter.post('/login', 
+    zValidator('json', loginSchema), 
+    async (c) => {
 
     // get the user data from body
     // validate the user data
